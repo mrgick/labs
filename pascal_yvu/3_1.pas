6a89297eccfe7da554219b7ea5d0daba3c731822
mrgick <45306973+mrgick@users.noreply.github.com>
2021-02-28 15:05:41 +0300

    Add files via upload

---
+++ pascal_yvu/3_1.pas
@@ -0,0 +1,10 @@
+program three;
+Uses math;
+var k: integer;
+var s: real;
+begin
+	s := 0;
+    for k := 1 to 30 do
+        s := s + power(k,0.3) + exp(0.3*k);
+    writeln('s=',s:0:6);
+end.
---
+++ pascal_yvu/3_2.pas
@@ -0,0 +1,14 @@
+program three;
+Uses math;
+var k: integer;
+var s: real;
+begin
+	s := 0;
+	k:=1;
+	while k<=30 do 
+	begin
+        s := s + power(k,0.3) + exp(0.3*k);
+        k := k+1;
+    end;
+    writeln('s=',s:0:6);
+end.
