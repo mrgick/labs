7b909d8ad36d6ca45d0b458a38b69ad257576a38
mrgick <45306973+mrgick@users.noreply.github.com>
2021-03-04 14:19:36 +0300

    fix!
    Former-commit-id: 0d7a7e51c3f1e5783098d6e1a80d4ddbdd8c6aa6

--- pascal_yvu/8.pas
+++ pascal_yvu/8.pas
@@ -1,33 +1,25 @@
 program eight;
 Uses Math;
-
 type mas = array[1..4] of real;
-
 const C: mas = (0.85, 1.4, 1.12, 3.24);
-const N: integer = 4;
-
+	  N: integer = 4;
 var X, Y, Z: real;
 
-Function F(C:mas; N:integer; switch:string; X,Y:real):real;
-	var Fak:real;
-	var i:integer;
-	begin
-		Fak:=1;
-		for i:=1 to N do
-			// костыль, но я ничего придумать другого не смог, но под тз подходит)
-			case switch of
-			'X':Fak:=C[i]/i;
-			'Y':Fak:=(C[i]-X)/power(i,2);
-			'Z':Fak:=(C[i]-Y)/power(i,3);
-			end;
-		F:=Fak;
-	end;
-
+Function F(C:mas; N:integer; U:real; j:integer):real;
+var Fak:real;
+var i:integer;
 begin
-	X := F(C,N,'X',0,0);
-	Y := F(C,N,'Y',X,0);
-	Z := F(C,N,'Z',X,Y);
-	writeln('X=',X:0:2,' Y=',Y:0:6,' Z=',Z:0:6);
+	Fak:=1;
+	for i:=1 to N do
+		Fak:=(C[i]-U)/power(i,j);
+	F:=Fak;
+end;
+	
+begin
+	X := F(C,N,0,1);
+	Y := F(C,N,X,2);
+	Z := F(C,N,Y,3);
+	writeln('X=',X:0:2,' Y=',Y:0:3,' Z=',Z:0:4);
 end.
 
 
