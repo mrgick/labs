08d86287ba88beebb95a60538bc4e5aefa57f649
mrgick <45306973+mrgick@users.noreply.github.com>
2021-03-09 14:48:31 +0300

    Add files via upload
    some redesign

--- pascal_yvu/3_2.pas
+++ pascal_yvu/3_2.pas
@@ -1,14 +1,13 @@
 program three;
 Uses math;
-var k: integer;
-    s: real;
+var k: integer; s: real;
 begin
 	s := 0;
-	k:=1;
+	k := 1;
 	while k<=30 do 
 	begin
         s := s + power(k,0.3) + exp(0.3*k);
         k := k+1;
     end;
-    writeln('s=',s:0:6);
+    writeln('s=',s:0:4);
 end.
