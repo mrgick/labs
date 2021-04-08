690ffe64ab552599e160a2f65f7aed0689feec5d
mrgick <45306973+mrgick@users.noreply.github.com>
2021-04-08 21:19:10 +0300

    Add files via upload
    Former-commit-id: be1dc3dce90f48415b87a684bdbc2ba770a7f147

--- pascal_yvu/6.pas
+++ pascal_yvu/6.pas
@@ -1,26 +1,21 @@
 program six;
 Uses Math;
-var E,i,j,S,l,SUM :real;
+var E,S,l,SUM :real;
+	i,j:integer;
 begin
     E := 0.001;
-    SUM := 0; //переменная внешней суммы
-    S := 0; //переменная внутренней суммы
-    l := 1; //нужен для проверки точности, так же указываем l>E, чтобы запустить цикл while
-    
-    i := 1;
-    while i<6 do begin
-	    //внутренний цикл
+    SUM := 0;
+    for i:=1 to 5 do begin
 	    l := 1;
 	    j := 1;
 	    S := 0;
-	    while l > E do begin
+	    while abs(l) > E do begin
 	        l := sqrt(sqr(i)+sqr(j))/power(i+j,3.8);
 	        S := S + l;
 	        j := j+1;
 	    end;
-	    //---------------
 	    SUM := SUM + (1/sin(i))*S;
-	    i := i + 1;
+		writeln(i,') SUM=',SUM:0:3, ', S=', S:0:3);
 	end;
-    writeln('SUM=',SUM:0:3);
+    writeln('In the total: SUM=',SUM:0:3);
 end.
