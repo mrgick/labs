d0a9c3fce467d363ce1fc4a36140ccbf5551195c
mrgick <45306973+mrgick@users.noreply.github.com>
2021-04-14 18:49:27 +0300

    fix bug
    there was a mistake
    Now It is good!)

--- pascal_yvu/8.pas
+++ pascal_yvu/8.pas
@@ -6,20 +6,20 @@ const C: mas = (0.85, 1.4, 1.12, 3.24);
 var X, Y, Z: real;
 
 Function F(C:mas; N:integer; U:real; j:integer):real;
-var Fak:real;
+var P:real;
 	i:integer;
 begin
-	Fak:=1;
+	P:=1;
 	for i:=1 to N do
-		Fak:=(C[i]-U)/power(i,j);
-	F:=Fak;
+		P:=P*(C[i]-U)/power(i,j);
+	F:=P;
 end;
 	
 begin
 	X := F(C,N,0,1);
 	Y := F(C,N,X,2);
 	Z := F(C,N,Y,3);
-	writeln('X=',X:0:2,' Y=',Y:0:3,' Z=',Z:0:4);
+	writeln('X=',X:0:4,' Y=',Y:0:4,' Z=',Z:0:4);
 end.
 
 
