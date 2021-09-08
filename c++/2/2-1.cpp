c756bb183c754f01a188122088d600e6db4928b2
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-08 22:32:35 +0300

    add 2-1 lab
    Former-commit-id: 29bd83664681ed505f31e876c1d76ab3914da807

--- c++/2/2-1.cpp
+++ c++/2/2-1.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    cout << "Enter three numbers:\n";
-    
+    double a, b, c;
+    cout << "Enter three positive numbers:\n";
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    if (a==0 || b==0 || c==0){
+		cout << "false";
+		return 0;
+	}
+
+    if (a<0 || b<0 || c<0){
+		cout << "Negative!";
+		return 0;
+	}
+
+    if (a<=b+c && b<=a+c && c<=a+b) {
+        cout << "true\n";
+    }
+    else {
+        cout << "false\n";
+    }
+
     cin.get();
     return 0;
 }
