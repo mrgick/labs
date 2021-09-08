d4dbf626b4289d20f05cacedda83f3d4bdc6f5e6
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-08 22:20:55 +0300

    working
    Former-commit-id: ccb89a7181a35bd6ae62c9340216e1344dcfe226

---
+++ c++/2/2-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter three numbers:\n";
+    
+    cin.get();
+    return 0;
+}
---
+++ c++/README.md
@@ -0,0 +1,1 @@
+Лабы сделаны по [методичке](https://ia601504.us.archive.org/0/items/metodichka-po-tp-khaggi-p-a_202109/Metodichka_po_TP_Khaggi_P_A.pdf)
