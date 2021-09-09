f345490a0d122fb1376f541c669493a954a2c16d
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-09 20:27:40 +0300

    start theme 3...
    Former-commit-id: 932bcb2629551eb2bcdd4fd5b0e83e4392ec22a2

---
+++ c++/3/3-1.cpp
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+int main() {
+    int n, sum = 0;
+    printf("Enter number:\n");
+    scanf("%d", &n);
+    for (int i = 2; i < n; i += 2) {
+        sum += i;
+    }
+    printf("Sum: %d\n", sum);
+    return 0;
+}
