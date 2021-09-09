80a7e970a2a9065df39d278be64581ba2eed2bf0
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-09 22:01:25 +0300

    add 3-7
    Former-commit-id: 96bea10ba423cd5902eda42255a78668e7aba1a3

---
+++ c++/3/3-7.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cmath>
+
+int main() {
+    int counter = 0;
+    for (int i = 100000; i <= 999999; i++){
+        if (i/100000 + i/10000%10 + i/1000%10 == 
+            i/100%10 + i/10%10 + i%10) {
+            counter++;
+        }
+    }
+    printf("%d\n",counter);
+    return 0;
+}
