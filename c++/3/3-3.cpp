3baf8a4e0371ca3655bbc8f11d4616c884af3c22
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-09 20:55:31 +0300

    add 3-3
    Former-commit-id: 0b40613fe8454b4b3b1447e5e04c1467bda23384

---
+++ c++/3/3-3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    double c;
+
+	printf("┌───┬──────────┐\n");
+    printf("│ F │    C     │\n");
+    printf("├───┼──────────┤\n");
+	for (int f = 0; f <= 300; f += 20){
+		c = (5.0/9.0) * (f-32);
+		printf("│%3d│%10.6lf│\n", f, c);
+	}
+    printf("└───┴──────────┘\n");
+
+}
