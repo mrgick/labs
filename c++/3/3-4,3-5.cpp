dfcd8c7c59930faf9377f458a3750610c027194d
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-09 21:06:59 +0300

    add 3-4 and 3-5 in one file xD))) Ох уж эти люди, которые используют pow, а не пробегаются по for...
    Former-commit-id: 4c38c2cd0b0d11355070072faec193017d58d62e

---
+++ c++/3/3-4,3-5.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cmath>
+
+int main() {
+    double x;
+	int n;
+	long double xn;
+	printf("Enter x and n separated by space or enter.\n");
+    scanf("%lf %d", &x, &n);
+    xn = pow(x, n);
+	printf("x^n = %llf\n", xn);
+}
