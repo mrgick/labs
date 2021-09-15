d8ae559ea58aa54f4b44d0a7043b3dcd42974915
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-15 21:56:49 +0300

    Я решил это!
    Former-commit-id: c819173419f2eb1dad4d4bb595007be79b9d6ccf

---
+++ 2_курс/численные_методы/1/1.py
@@ -0,0 +1,17 @@
+import math
+
+def φ(x):
+    return x + 2*(math.log(x)-x+1.8)
+
+x = 2
+Δ = 3
+ε = 10**(-5)
+
+
+while True:
+    Δ = φ(x)
+    if abs(x-Δ) < ε:
+        break
+    x = Δ
+    print(x, Δ)    
+
