17cb0aa37ec3de7eedc3fb7f6899a06383991855
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-15 22:08:55 +0300

    add main
    Former-commit-id: aa637144644e5ce225a0c382d2e413f0f4e819b0

--- 2_курс/численные_методы/1/1.py
+++ 2_курс/численные_методы/1/1.py
@@ -5,14 +5,17 @@ def φ(x):
     return x + 2*(math.log(x)-x+1.8)
 
 
-x = 2
-Δ = 3
-ε = 10**(-5)
+def main():
+    x = 2
+    ε = 10**(-5)
+
+    while True:
+        Δ = φ(x)
+        if abs(x-Δ) < ε:
+            break
+        x = Δ
+        print(x, Δ)
 
 
-while True:
-    Δ = φ(x)
-    if abs(x-Δ) < ε:
-        break
-    x = Δ
-    print(x, Δ)
+if __name__ == "__main__":
+    main()
