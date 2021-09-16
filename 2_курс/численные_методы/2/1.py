f058468d98f4771b193e3948dbd2ad8fec6b3c6a
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-16 15:32:10 +0300

    add 2
    Former-commit-id: 766f2bbd0b44f32c7516a9ede745c615e151d69e

---
+++ 2_курс/численные_методы/2/1.py
@@ -0,0 +1,35 @@
+import math
+
+
+def f(x):
+    """
+        10ln(x) + 0.2x^2 - 4.5x + 1 = 0
+    """
+    return 10 * math.log(x) + 0.2 * pow(x, 2) - 4.5 * x + 1
+
+
+def half_divide(a, b, ε):
+    while b - a > ε:
+        x = (a + b) / 2
+        if f(a) * f(x) <= 0:
+            b = x
+        else:
+            a = x
+    return x
+
+
+def main():
+    ε = 10**(-3)
+    a = 1
+    b = 18
+    step = (b - a) / 20
+    while a < b:
+        if f(a) * f(a + step) <= 0:
+            print(f"a={a:.{7}f} b={a+step:.{7}f}")
+            x = half_divide(a, a + step, ε)
+            print(f"x={x:.{7}f}")
+        a += step
+
+
+if __name__ == "__main__":
+    main()
