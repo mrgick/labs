6000447d3824db6956ca9d0c96aa6970df2f293a
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-19 18:45:05 +0300

    add 7-1
    Former-commit-id: b364eceb47f22f1f8f7feb968617975454dc1a3c

---
+++ 2_курс/технологии_программирования/7/7-1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cctype>
+
+/*
+English alphabet:
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+*/
+
+bool is_vowel(int j)
+{
+    std::string vowels = "aeouiAEOUI";
+    return vowels.find(j) != std::string::npos;
+}
+
+bool is_consonant(int j)
+{
+    std::string consonant = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ";
+    return consonant.find(j) != std::string::npos;
+}
+
+int main()
+{
+    std::string vowels = "aeouiAEOUI";
+    std::string text;
+    std::getline(std::cin, text);
+    std::cout << "Vowels:" << count_if(text.begin(), text.end(), is_vowel) << "\n";
+    std::cout << "Consonant:" << count_if(text.begin(), text.end(), is_consonant) << "\n";
+    std::cout << "Upper:" << count_if(text.begin(), text.end(), isupper) << "\n";
+    std::cout << "Lower:" << count_if(text.begin(), text.end(), islower) << "\n";
+    return 0;
+}
