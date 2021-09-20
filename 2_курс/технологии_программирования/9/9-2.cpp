6c9d435018e44eb4fe6f0155598880fd32b6fab2
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-20 22:07:50 +0300

    refactoring
    Former-commit-id: 708ea015ef481c3a6f2a26f6ab7d2173225ec0c5

--- 2_курс/технологии_программирования/9/9-2.cpp
+++ 2_курс/технологии_программирования/9/9-2.cpp
@@ -4,13 +4,14 @@
 int main()
 {
     std::ifstream f1("test1.txt");
-    std::ofstream f2("test2.txt");
+    std::ofstream f2("test2.out");
     std::string tmp, word;
     getline(std::cin, word);
 
-    while (!f1.eof())
+    while (true)
     {
         getline(f1, tmp);
+        if (f1.eof()) break;
         if (tmp.find(word) != std::string::npos)
         {
             f2 << tmp << std::endl;
