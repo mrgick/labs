c454623b032c04d32a2df2ab1bda00da019d8591
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-20 22:11:01 +0300

    rename .txt to .out
    Former-commit-id: f8d16ed05535d7f6ccf7d050df68c90ceb9a1e20

--- 2_курс/технологии_программирования/9/9-7.cpp
+++ 2_курс/технологии_программирования/9/9-7.cpp
@@ -41,16 +41,9 @@ int main()
     std::ifstream f1;
     std::ofstream f2;
     f1.open("test1.txt", std::ios::binary);
-    f2.open("test2.out", std::ios::binary);
+    f2.open("test2.txt", std::ios::binary);
 
-    word word1, word2;
 
-    while (!f1.eof())
-    {
-        word1 = find_next_word(f1);
-        word2 = find_next_word(f1);
-        f2 << word2.self + word1.other + word1.self + word2.other;
-    }
 
     f1.close();
     f2.close();
