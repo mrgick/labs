fac301a456900492bac85b247913d31fbdf9dbbd
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-20 17:02:42 +0300

    add 9-1
    Former-commit-id: c9470b1e4d2c0aa497a5450167c439ed7711fbb6

---
+++ 2_курс/технологии_программирования/9/9-1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream> 
+#include <cstring>
+
+/*
+Использую getline, чтобы не было скопировано два раза
+последнее значение. Подробнее: https://vk.cc/c65kwo
+*/
+
+int main()
+{
+    char filename[30];
+	std::cin >> filename;
+    std::ifstream f1("test1.txt");
+    std::ofstream f2(filename);
+    char ch;
+    char input_line[81];
+	while (f1.getline(input_line, 80)) {
+		f2 << input_line << std::endl;
+	}
+    f1.close();
+	f2.close();
+    return 0;
+}
