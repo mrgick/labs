6832880ae8172d5020723c06cd283714c0b568b9
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-20 19:12:41 +0300

    add 9-3
    Former-commit-id: c1e917a66a292a926128441005348df16afe98c7

---
+++ 2_курс/технологии_программирования/9/9-3.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+
+std::string find_next_word(std::ifstream &file)
+{
+    std::string word = std::string();
+    char tmp;
+    while (true)
+    {
+        if (file.eof())
+        {
+            break;
+        }
+
+        file.get(tmp);
+
+        if (isalpha(tmp) or isdigit(tmp))
+        {
+            word += tmp;
+        }
+        else if (word != std::string())
+        {
+            break;
+        }
+    }
+    return word;
+}
+
+int main()
+{
+    std::ifstream f1;
+    f1.open("test1.txt", std::ios::binary);
+
+    std::string word, longest = std::string();
+    char tmp;
+
+    while (!f1.eof())
+    {
+        word = find_next_word(f1);
+        if (word.length() > longest.length())
+        {
+            longest = word;
+        }
+    }
+
+    f1.clear();
+    f1.seekg(0, std::ios::beg);
+    int counter = 0;
+
+    while (!f1.eof())
+    {
+        word = find_next_word(f1);
+        if (word == longest)
+        {
+            counter++;
+        }
+    }
+
+    f1.close();
+    std::cout << "The longest is " 
+              << longest 
+              << ", counter = " 
+              << counter 
+              << std::endl;
+    return 0;
+}
