99a2132dcbd6324bf5ae79ccbe77b158ded69350
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-20 16:23:38 +0300

    add 8 lab
    Former-commit-id: 30bd706f0700edb20c648fc54cc6562ff5a244af

---
+++ 2_курс/технологии_программирования/8/8-2.cpp
@@ -0,0 +1,71 @@
+#include "stdio.h"
+#include <limits>
+
+int fib(int n)
+{
+    int f1 = 0;
+    int f2 = 1;
+    int fn;
+    if (n < 1)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    if (n > 1)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            fn = f1 + f2;
+            f1 = f2;
+            f2 = fn;
+        }
+    }
+    return fn;
+}
+
+int fib_rec(int n)
+{
+    switch (n)
+    {
+    case 0:
+        return 0;
+    case 1:
+        return 1;
+    default:
+        return fib_rec(n - 1) + fib_rec(n - 2);
+    }
+}
+
+void cout_max_fib_n_in_int()
+{
+    int imax = std::numeric_limits<int>::max();
+    printf("Max int = %d\n", imax);
+    long int f1 = 0,
+             f2 = 1,
+             fn = 1,
+             n = 1;
+    while (fn < imax)
+    {
+        fn = f1 + f2;
+        f1 = f2;
+        f2 = fn;
+        n++;
+        //printf("%d %d\n", fn, n);
+    }
+    n -= 1;
+    printf("Max fibonacci N = %d\nfibonacci(N) = %d\n", n, f1);
+}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    printf("fib_rec(N) = %d\n", fib_rec(N));
+    printf("fib(N) = %d\n", fib(N));
+    printf("Finding max fibonacci N:\n");
+    cout_max_fib_n_in_int();
+    return 0;
+}
