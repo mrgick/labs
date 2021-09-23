3a37f069ce035af6417993e9e66852a27e682c70
mrgick <45306973+mrgick@users.noreply.github.com>
2021-09-23 16:35:46 +0300

    some refactoring)

--- 2_курс/инженерная_графика/1/graphic.js
+++ 2_курс/инженерная_графика/1/graphic.js
@@ -8,7 +8,7 @@ function Graphic() {
             labels: [],
             datasets: [{
                 label: "x^2",
-                data: get_data(-6, 6, 1, 'x^2'),
+                data: get_data(-10, 10, 1, 'x^2'),
                 backgroundColor: 'rgb(255, 99, 132)',
                 borderColor: 'rgb(255, 99, 132)',
                 borderWidth: 1
@@ -35,25 +35,38 @@ function get_data(x_start, x_end, step, math_formula) {
 }
 
 function add_graphic() {
+    try {
+        var formData = $('form').serializeArray()
+        var data = {}
+        for (var i = 0; i < formData.length; i++) {
+            data[formData[i].name] = formData[i].value;
+        }
 
-    var math_formula = 'sin(x)'
-    var x_start = -6
-    var x_end = 6
-    var step = 1
-    var color = 'rgb(43, 99, 132)'
+        console.log(data)
+        var math_formula = data["math_formula"]
+        var x_start = parseInt(data["x_start"], 10)
+        var x_end = parseInt(data["x_end"], 10)
+        var step = parseInt(data["x_step"], 10)
+        var color = data["graphic_color"]
 
 
-    var dataset = {
-        label: math_formula,
-        data: get_data(x_start, x_end, step, math_formula),
-        backgroundColor: color,
-        borderColor: color,
-        borderWidth: 1
+        var dataset = {
+            label: math_formula,
+            data: get_data(x_start, x_end, step, math_formula),
+            backgroundColor: color,
+            borderColor: color,
+            borderWidth: 1
+        }
+
+        myChart.data.datasets.push(dataset)
+        myChart.update()
     }
-
-    myChart.data.datasets.push(dataset)
-    myChart.update()
+    catch (err) {
+        console.error(err)
+        alert(err)
+    }
 }
+
 
 
 
