6275ce8451482dbeb30a569f682d66268dcb2e29
mrgick <45306973+mrgick@users.noreply.github.com>
2021-10-13 17:38:12 +0300

    delete function grad

--- 2_курс/численные_методы/12/1.js
+++ 2_курс/численные_методы/12/1.js
@@ -2,7 +2,7 @@ function main() {
     let x = -5, y = -5,
         eps = 10 ** (-2) // здесь должно быть -2 по условию, но при -3 или -4 значение лучше, следует поиграться
 
-    let h = 1, dx=0, dy=0, g = eps, i = 0
+    let h = 1, dx=0, dy=0, g = eps, i = 0, grad = 0
     do {
         i++
         x = x - h * dx
@@ -10,13 +10,10 @@ function main() {
         F = f(x, y)
         dx = (f(x + g, y) - f(x, y)) / g
         dy = (f(x, y + g) - f(x, y)) / g
-        console.log(`i=${i.toFixed()}, x=${x.toFixed(3)}, y=${y.toFixed(3)}, df/dx=${dx.toFixed(3)}, df/dy=${dy.toFixed(3)}, grad=${grad(x,y).toFixed(3)}, f(x,y)=${f(x,y).toFixed(3)}`)
-    } while (Math.abs(grad(dx, dy)) > eps);
+        grad = Math.sqrt(dx ** 2 + dy ** 2)
+        console.log(`i=${i.toFixed()}, x=${x.toFixed(3)}, y=${y.toFixed(3)}, df/dx=${dx.toFixed(3)}, df/dy=${dy.toFixed(3)}, grad=${grad.toFixed(3)}, f(x,y)=${f(x,y).toFixed(3)}`)
+    } while (Math.abs(grad) > eps);
     console.log(`Amount of iterations:${i.toFixed()}, x=${x.toFixed(3)}, y=${y.toFixed(3)}, f(x,y)=${f(x,y).toFixed(3)}`)
-}
-
-function grad(dx, dy) {
-    return Math.sqrt(dx ** 2 + dy ** 2)
 }
 
 function f(x, y) {
