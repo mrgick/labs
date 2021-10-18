bc6cd0696f91353f176d6d5d23adc9d46443aedd
mrgick <45306973+mrgick@users.noreply.github.com>
2021-10-18 15:52:52 +0300

    fixed Теперь всё, как в блок-схеме

--- 2_курс/численные_методы/4/1.js
+++ 2_курс/численные_методы/4/1.js
@@ -7,35 +7,32 @@ function main() {
 
     let a = 0,
         b = 1.5,
-        e = 0.00001,
+        e = 10 ** -5,
         N = 2
 
     let z = f(a) - f(b),
         counter = 0,
-        I_old = e + 1,
+        I_old,
         I_new = 0,
         h,
-        odd, // нечётное
-        even // чётное
+        even = 0,            // чётное
+        odd = f((a + b) / 2) // нечётное
 
-    for (; Math.abs(I_new - I_old) > e; N *= 2) {
-        odd = 0
-        even = 0
-
+    do {
+        even += odd
         h = (b - a) / N
-
+        odd = 0
         for (let i = 1; i <= N - 1; i += 2) {
-            even += f(a + h * i)
-            odd += f(a + h * (i + 1))
+            odd += f(a + h * i)
         }
-
         I_old = I_new
         I_new = (h / 3) * (z + 4 * even + 2 * odd)
         counter++
-        
+        N *= 2
         console.log(`n = ${counter} I_old = ${I_old.toFixed(5)} I_new = ${I_new.toFixed(5)}`)
-    }
-    console.log(`Answer = ${I_new}`)
+    } while (Math.abs(I_new - I_old) > e) 
+
+    console.log(`Answer = ${I_new.toFixed(5)}`)
 }
 
 
