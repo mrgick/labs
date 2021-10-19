f5813a62294d19eebf387a7957f5f35e6859fa00
mrgick <45306973+mrgick@users.noreply.github.com>
2021-10-19 17:23:45 +0300

    refactoring!

--- 2_курс/численные_методы/5/1.js
+++ 2_курс/численные_методы/5/1.js
@@ -3,50 +3,53 @@ function f(x) {
 }
 
 function sympson(b, e) {
-    let n = 2,
-        a = 0.1,
-        Sum_odd, // нечетное
-        Sum_even, // четное
-        I_old,
-        h, z
 
-    Sum_odd = 0
-    I_new = 0
-    z = f(a) + f(b)
+    let a = 0.1,
+        N = 2
+
+    let z = f(a) + f(b),
+        I_old,
+        I_new = 0,
+        h,
+        even = 0,            // чётное
+        odd = f((a + b) / 2) // нечётное
 
     do {
-        h = (b - a) / n
-        Sum_even = 0
-        for (let i = 1; i <= n - 1; i += 2) {
-            Sum_even += f(a + i * h);
+        even += odd
+        h = (b - a) / N
+        odd = 0
+        for (let i = 1; i <= N - 1; i += 2) {
+            odd += f(a + h * i)
         }
-        Sum_odd += Sum_even
         I_old = I_new
-        I_new = (h / 3) * (z + 4 * Sum_even + 2 * Sum_odd)
-        n *= 2
-    } while (Math.abs(I_old - I_new) > e)
-
-    return I_old
-
+        I_new = (h / 3) * (z + 4 * even + 2 * odd)
+        N *= 2
+    } while (Math.abs(I_new - I_old) > e)
+    return I_new
 }
+
 
 function main() {
     let a = 0.2,
         b = 8,
-        e = 0.001
+        e = 0.01,
+        e1 = 0.2
 
     let i = 0,
         x
 
     do {
         x = (a + b) / 2
-        if ((sympson(a, e) * sympson(x, e)) < 0)
+        if ((sympson(a, e1) * sympson(x, e1)) < 0)
             b = x
         else
             a = x
-        console.log(`i = ${i} a = ${a} f(a) = ${sympson(x, e)}`)
+        console.log(`i = ${i} a = ${a.toFixed(4)} f(a) = ${sympson(x, e).toFixed(5)}`)
         i++
+        e1 = 0.47 * e1
+        //console.log(e1);
     } while (Math.abs(b - a) > e)
 }
+
 
 main()
