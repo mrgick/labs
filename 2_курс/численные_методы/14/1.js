b6a7e08907548d35f9a7bb39b27cca0d4c5b1400
mrgick <45306973+mrgick@users.noreply.github.com>
2021-10-27 17:49:26 +0300

    refactoring

--- 2_курс/численные_методы/14/1.js
+++ 2_курс/численные_методы/14/1.js
@@ -1,29 +1,38 @@
 function main() {
-    let eps = 0.1, N = 20, a = [0.004] // из-за расхождения пришлось подрегулировать a[0], x[0] и y[0]
-    let x = [-0.3], y = [-0.3], F = []
-    let i, k = 0
-    do {
-        k++
-        F[0] = f(x[0], y[0])
+    let eps = 0.1, N = 20, a0 = 0.1
+    let x0 = -0.3, y0 = -0.3
+    let n = 0, i = 0
+    let ak, F, F_old, xn
+
+    while (true) {
+        gradient = grad(x0, y0)
+        if (Math.abs(gradient) <= eps) break;
+
         i = 1
-        do {
-            F[1] = F[0]
-            x[1] = x[0] - i * a[0] * f_dx(x[0], y[0])
-            y[1] = y[0] - i * a[0] * f_dy(x[0], y[0])
-            F[0] = f(x[1], y[1])
+        F = f(x0, y0)
+        while (true) {
+            xn = x0 - i * a0 * f_dx(x0, y0)
+            yn = y0 - i * a0 * f_dy(x0, y0)
+            F_old = F
+            F = f(xn, yn)
+            if (F > F_old) break;
             i++
-        } while (F[1] > F[0])
-        if (k > N) break
-        a[k] = a[0] * (i - 1)
-        if (a[1] > 0) {
-            x[0] = x[0] - a[k] * f_dx(x[0], y[0])
-            y[0] = y[0] - a[k] * f_dy(x[0], y[0])
-            grad = f_dx(x[0], y[0]) + f_dy(x[0], y[0])
-            console.log(`${k})x=${x[0].toFixed(2)}, y=${y[0].toFixed(2)}, f(x,y)=${f(x[0],y[0]).toFixed(2)}, grad=${grad.toFixed(2)}, a[k]=${a[k].toFixed(2)}`);
-        } else {
-            a[0] = a[0] / 10
         }
-    } while (f_dx(x[0], y[0]) ** 2 + f_dy(x[0], y[0]) ** 2 > eps**2)
+        
+        if (n >= N) break;
+
+        ak = a0 * (i - 1);
+        if (ak > 0) {
+            x0 = x0 - ak * f_dx(x0, y0)
+            y0 = y0 - ak * f_dy(x0, y0)
+            n++
+            console.log(`${n})x=${x0.toFixed(4)}, y=${y0.toFixed(4)}, f(x,y)=${f(x0, y0).toFixed(4)}, grad=${grad(x0, y0).toFixed(4)}, ak=${ak.toFixed(2)}`)
+        }
+        else {
+            a0 /= 10
+        }
+    }
+
 }
 
 function f(x, y) {
@@ -36,6 +45,10 @@ function f_dx(x, y) {
 
 function f_dy(x, y) {
     return 0.9 + 0.35 * 2 * y * Math.exp(5.76 * x ** 2 + 0.35 * y ** 2)
+}
+
+function grad(x, y) {
+    return (f_dx(x, y) ** 2 + f_dy(x, y) ** 2) ** 0.5
 }
 
 main()
